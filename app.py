e96f8760ed31a54c0575a35ea8a743c17fd05596
sarahoeri <oerisarah@gmail.com>
2020-01-24 16:23:56 +0300

    git commit -m "initial commit"

---
+++ app.py
@@ -0,0 +1,44 @@
+# Variables
+character_name = "John"
+character_age = "50"
+isMale = True
+
+print("there was once a man called " + character_name)
+print("he was at an age of " + character_age)
+
+# Functions
+phrase = "Giraffe Academy"
+print(phrase + " is developed now")
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[9])
+print(phrase.index("raffe"))
+print(phrase.replace("Academy", "School"))
+
+# Numbers
+print(5 * (4 +5))
+
+myNum = 10
+print(str(myNum) + " is my favourite number")
+my_number = -0.54
+print(abs(my_number))
+print(pow(10, 8))
+# to find max and min values
+print(max(27, 88, 73, 109))
+print(round(3.9))
+
+from math import *
+print(floor(9.5))
+print(ceil(7.2))
+print(sqrt(36))
+
+# getting input from users
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Heeey " + name + "!" " You are " + age)
+
+
+
